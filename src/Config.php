c3b4f10ed28b046577eeb597a8946a53145d4960
fe3dback <fe3dback@yandex.ru>
2017-03-03 00:37:39 +0300

    remove test stuff

--- src/Config.php
+++ src/Config.php
@@ -13,58 +13,14 @@
 namespace Kitrix\Config;
 
 use Kitrix\Config\API\Register;
-use Kitrix\Config\Fields\Checkbox;
-use Kitrix\Config\Fields\Input;
-use Kitrix\Config\Fields\Select;
-use Kitrix\Config\Fields\Textarea;
 use Kitrix\Config\ORM\ValuesTable;
 use Kitrix\MVC\Admin\RouteFactory;
 use Kitrix\Plugins\Plugin;
 
 final class Config extends Plugin
 {
-    public function run2()
+    public function run()
     {
-        $test = ConfRegistry::makeField(Input::class, 'test')
-            ->setTitle('Тестовое поле')
-            ->setDefaultValue("default test")
-            ->setDisabled(true);
-
-        $cb = ConfRegistry::makeField(Checkbox::class, 'cb')
-            ->setTitle('Чек бокс')
-            ->setHelpText("<b>Important!</b>: this is example of help message..");
-
-        $anotherTest = ConfRegistry::makeField(Input::class, 'second')
-            ->setTitle('Еще одно поле для теста')
-            ->setHidden(true);
-
-        $someMessage = ConfRegistry::makeField(Input::class, 'LOLTEST1')
-            ->setTitle('lol test')
-            ->setDefaultValue('hello!')
-            ->setReadonly(true);
-
-        $group = ConfRegistry::makeGroup('Основные настройки', Config::class)
-            ->addField($test)
-            ->addField($cb)
-            ->addField($anotherTest)
-            ->addField($someMessage)
-            ->addField(
-                ConfRegistry::makeField(Textarea::class, 'text')
-                    ->setTitle("text area")
-            )
-            ->addField(
-                ConfRegistry::makeField(Select::class, 'select_test')
-                    ->setTitle("Пример селекта")
-                    ->setDefaultValue(0)
-                    ->setOptions(["world" => "Hello world", "shit" => "Bitrix"])
-            );
-
-        $group2 = ConfRegistry::makeGroup('2 настройки', Config::class)
-            ->addField($test);
-
-        ConfRegistry::registerGroup($group);
-        ConfRegistry::registerGroup($group2);
-
         // load db fields
         Register::getInstance()->loadFields();
     }
@@ -87,10 +43,6 @@ final class Config extends Plugin
                 ->setTitle($group->getTitle())
                 ->setIcon('fa-tag');
         }
-
-        $configRoutes[] = RouteFactory::makeRoute('/list/', SiteConfigController::class, 'configList')
-            ->setTitle('Собственные настройки сайта')
-            ->setIcon('fa-tags');
 
         return $configRoutes;
     }
